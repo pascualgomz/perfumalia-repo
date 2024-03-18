84053be4233d5909667f2077b17ca0d02d2083b4
unknown <ezapataa1@eafit.edu.co>
2024-03-17 23:59:10 -0500

    all user-side urls and templates added

--- perfumalia/store/views.py
+++ perfumalia/store/views.py
@@ -3,5 +3,38 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
-class homePageView(TemplateView):
+class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class LoginPageView(TemplateView):
+    template_name = 'login.html'
+
+class SingUpPageView(TemplateView):
+    template_name = 'singup.html'
+
+class ProfilePageView(TemplateView):
+    template_name = 'user.html'
+
+class PerfumesPageView(TemplateView):
+    template_name = 'perfumes.html'
+
+class PerfumesDetailsPageView(TemplateView):
+    template_name = 'perfumesdetails.html'
+
+class CartPageView(TemplateView):
+    template_name = 'cart.html'
+
+class OrdersPageView(TemplateView):
+    template_name = 'orders.html'
+
+class OrderDetailsPageView(TemplateView):
+    template_name = 'orderdetails.html'
+
+class PaymentPageView(TemplateView):
+    template_name = 'payment.html'
+
+class SearchResultsPageView(TemplateView):
+    template_name = 'searchresults.html'
+
+class SubscriptionPageView(TemplateView):
+    template_name = 'subscription.html'
